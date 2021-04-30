5bead58ad2043b701c4bc2fb9e4dc9213f3fa54e
alex <contact@alex-vicol.com>
2021-04-30 09:32:30 +0100

    Created component Song and  Player...State added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+//Import StyLes
+import './styles/app.scss';
+//Import Components
+import Song from './components/song/song.jsx';
+import Player from './components/player/player.jsx';
+//Import State
+import data from "./data";
 
 function App() {
+  const [songs, setSongs] =useState(data());
+  const [ currentSong, setCurrentSong] =useState(songs[0])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Song currentSong = {currentSong} />
+
+        <Player/>
     </div>
   );
 }
